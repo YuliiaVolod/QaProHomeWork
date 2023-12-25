a6c90b4e5eae7b99f7f67aec2032fd4f57a03007
YuliiaVolod <stepaika74@gmail.com>
2023-12-25 20:44:35 +0200

    Кончила

--- HW05.js
+++ HW05.js
@@ -1,15 +1,15 @@
 const unit = "год";
 const value = 1.5;
 
-switch(unit) {
+switch (unit) {
     case "кг":
         console.log(value + "кг це " + value * 1000 + "гр");
         break;
     case "км":
-        console.log(value + "км це " + value * 1000 + "м");      
+        console.log(value + "км це " + value * 1000 + "м");
         break;
     case "год":
-        console.log(value + "год це " + value * 60 + "хв");    
+        console.log(value + "год це " + value * 60 + "хв");
         break;
     default:
         console.log("Некоректна одиниця!");
