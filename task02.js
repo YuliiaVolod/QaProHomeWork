f0c123eb8b9bdc6613991b477da505445e46d439
Yuliia <stepaika74@gmail.com>
2024-01-11 19:19:12 +0200

    Вартість послуг

--- task02.js
+++ task02.js
@@ -1,3 +1,51 @@
-/*створити метод price(), який обчислює та повертає загальну вартість "наданих послуг".
+/*
+створити метод price(), який обчислює та повертає загальну вартість "наданих послуг".
 створити метод minPrice() - який повертає мінімальний price
-створити метод maxPrice() - який повертає максимальний price*/
+створити метод maxPrice() - який повертає максимальний price
+*/
+
+const services = {
+	стрижка: 60,
+	гоління: 80,
+	"Миття голови": 100
+};
+
+services.price = function(){
+    let sum = 0; 
+    for(let key in this){
+        if(key !== "price" && key !== "minPrice" && key !== "maxPrice")
+            sum += this[key];
+    }
+    return sum;
+}
+
+services.minPrice = function(){
+    let min = Number.MAX_VALUE; 
+    for(let key in this){
+        if(key !== "price" && key !== "minPrice" && key !== "maxPrice"){
+            if(min > this[key]){
+                min = this[key];
+            }
+        }
+    }
+    return min;
+}
+
+services.maxPrice = function(){
+    let max = Number.MIN_VALUE; 
+    for(let key in this){
+        if(key !== "price" && key !== "minPrice" && key !== "maxPrice"){
+            if(max < this[key]){
+                max = this[key];
+            }
+        }
+    }
+    return max;
+}
+
+services["Розбити скло"] = 200;
+
+console.log("Сума: " + services.price());
+console.log("Мінімальна ціна: " + services.minPrice());
+console.log("Максимальна ціна: " + services.maxPrice());
+
